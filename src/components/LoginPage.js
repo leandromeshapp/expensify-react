8cfa779697e78bb7413de9e65e735b736cfc11f8
Leandro <leandro@Rodrigos-iMac.local>
2018-03-09 17:54:50 +0000

    style login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,12 @@ import { startLogin } from "../actions/auth"
 
 
 export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}> LOGIN </button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title"> Expensify </h1>
+            <p> It's time to get your expenses under control. </p>
+            <button class="button" onClick={startLogin}> Login  </button>
+        </div>
     </div>
 )
 
